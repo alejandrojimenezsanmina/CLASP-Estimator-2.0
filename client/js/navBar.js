2d32375f8e88dbf9a32d4d863c8c6266b686ea69
Jimenez Mendoza <alejandro.jimenez@sanmina.com>
2021-02-02 01:54:42 -0600

    sheet metal 100% now for real

--- client/js/navBar.js
+++ client/js/navBar.js
@@ -4,6 +4,9 @@ $( document ).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.modal').modal();
     $('.sidenav').sidenav();
+
+    let queryString = window.location.search;
+    console.log(queryString);
     
     var restart = document.querySelector("#restart")
     restart.addEventListener("click", function (){
@@ -14,12 +17,16 @@ $( document ).ready(function(){
     var infoButton = document.querySelector("#infoButton");
     infoButton.addEventListener("click", function (e){ e.target.classList.remove("glow")})
 
-    var dollarSign = document.querySelector('#slideCeption');
-    dollarSign.addEventListener("click", function (e) { 
-      if(dollarSign.classList.contains('glowGreen')){
-        dollarSign.classList.remove("glowGreen")
-      }
-    })
+    if (document.querySelector('#slideCeption')){
+      
+      var dollarSign = document.querySelector('#slideCeption');
+      dollarSign.addEventListener("click", function (e) { 
+        if(dollarSign.classList.contains('glowGreen')){
+          dollarSign.classList.remove("glowGreen")
+        }
+      })
+
+    }
 
     var qims;
     if (localStorage.getItem("projNum")){
